c2ceadf31533d822558de970c001e8ce3ea8ad9c
Jason Jan Ngo <ngo.jason@gmail.com>
2022-02-01 16:31:34 +0800

    load Tagalog data

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,20 +9,29 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        This is an open source clone of the game Wordle -{' '}
+        This is an open source clone of the game Wordle forked from -{' '}
         <a
           href="https://github.com/hannahcode/wordle"
           className="underline font-bold"
         >
-          check out the code here
-        </a>{' '}
-        and{' '}
-        <a
+          hannahcode
+        </a>. Current source is <a
+          href="https://github.com/jgngo/wordle"
+          className="underline font-bold"
+        >
+          jgngo
+        </a> 
+        
+        <p className="py-4">Tagalog word list from <a className="underline font-bold" href="https://tagalog.pinoydictionary.com/">https://tagalog.pinoydictionary.com</a> 
+        </p>
+
+        <p className="py-4"><a
           href="https://www.powerlanguage.co.uk/wordle/"
           className="underline font-bold"
         >
-          play the original here
+          Play original Wordle here
         </a>
+        </p>
       </p>
     </BaseModal>
   )
